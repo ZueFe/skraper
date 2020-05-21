3f67ab5139de49856227a671854f985cc5f56e5a
sokomishalov <sokomishalov@mail.ru>
2020-05-21 15:51:14 +0300

    telegram bot further progress

--- telegram-bot/src/main/kotlin/ru/sokomishalov/skraper/bot/telegram/config/SkraperBotConfig.kt
+++ telegram-bot/src/main/kotlin/ru/sokomishalov/skraper/bot/telegram/config/SkraperBotConfig.kt
@@ -1,9 +1,11 @@
 package ru.sokomishalov.skraper.bot.telegram.config
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import ru.sokomishalov.skraper.Skraper
 import ru.sokomishalov.skraper.SkraperClient
+import ru.sokomishalov.skraper.bot.telegram.autoconfigure.BotProperties
 import ru.sokomishalov.skraper.client.ktor.KtorSkraperClient
 import ru.sokomishalov.skraper.knownList
 
@@ -11,7 +13,8 @@ import ru.sokomishalov.skraper.knownList
  * @author sokomishalov
  */
 @Configuration
-class SkraperConfig {
+@EnableConfigurationProperties(BotProperties::class)
+class SkraperBotConfig {
     @Bean
     fun ktorClient(): SkraperClient = KtorSkraperClient()
 
